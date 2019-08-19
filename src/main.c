f85e17cfded5ac4347fa723205ea7e08ab87adf3
johnejardine <john.e.jardine@gmail.com>
2019-08-19 22:25:06 +0000

    Updated Makefile to embed build version info into binary

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 int main(int argc, char *args[]) {
-   printf("Build version:%i_%s\n","""JENKINS_BUILD_NO""", """GIT_COMMIT_HASH""");
+   printf("Build version:%04i_%.8s\n", BUILD_NO, COMMIT_HASH);
    printf("There are %i args\n", argc);
 }
